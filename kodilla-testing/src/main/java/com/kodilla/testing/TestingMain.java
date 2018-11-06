09ae123ff6244ae7cad54f7f3d6b0782232a2dec
Michał Kleszczewski <michalkleszczewski@macbook-pro-micha.home>
2018-11-06 17:14:21 +0100

    task 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        if ( calculator.add(5,7) == 5 + 7 ) {
+            System.out.println("test dodawania prawidłowy");
+        } else {
+            System.out.println("blad testu dodawania");
+        }
+        if ( calculator.subtract(11,6) == 11 - 6 ) {
+            System.out.println("test odejnowania prawidłowy");
+        } else {
+            System.out.println("blad testu odejmowania");
+        }
     }
 }
