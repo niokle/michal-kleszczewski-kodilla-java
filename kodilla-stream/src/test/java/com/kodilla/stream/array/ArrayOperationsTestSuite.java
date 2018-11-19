f1464dec0aa49366895da0c7059d89e915216110
Michał Kleszczewski <michalkleszczewski@macbook-pro-micha.home>
2018-11-19 21:06:20 +0100

    task 7.5 commit 2

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -7,14 +7,7 @@ public class ArrayOperationsTestSuite {
     @Test
     public void testGetAverage() {
         //Given
-        int[] numbers = new int[7];
-        numbers[0] = 0;
-        numbers[1] = 1;
-        numbers[2] = 2;
-        numbers[3] = 3;
-        numbers[4] = 4;
-        numbers[5] = 5;
-        numbers[6] = 6;
+        int numbers[] = {0,1,2,3,4,5,6};
 
         //When
         double result = ArrayOperations.getAverage(numbers);
