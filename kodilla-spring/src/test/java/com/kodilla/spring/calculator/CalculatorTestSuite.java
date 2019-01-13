3885af988d53a597cbbbdc8d81592b5b4f74c9e4
Michał Kleszczewski <michalkleszczewski@macbook-pro-micha.home>
2019-01-13 21:50:48 +0100

    task 14.2 commit 1

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,62 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Test
+    public void testCalculationsAdd() {
+        //given
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = applicationContext.getBean(Calculator.class);
+        //when
+        int a = 10;
+        int b = 5;
+        //then
+        Assert.assertEquals(15, calculator.add(a, b), 0);
+    }
+
+    @Test
+    public void testCalculationsSub() {
+        //given
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = applicationContext.getBean(Calculator.class);
+        //when
+        int a = 10;
+        int b = 5;
+        //then
+        Assert.assertEquals(5, calculator.sub(a, b), 0);
+    }
+
+    @Test
+    public void testCalculationsMul() {
+        //given
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = applicationContext.getBean(Calculator.class);
+        //when
+        int a = 10;
+        int b = 5;
+        //then
+        Assert.assertEquals(50, calculator.mul(a, b), 0);
+    }
+
+    @Test
+    public void testCalculationsDiv() {
+        //given
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = applicationContext.getBean(Calculator.class);
+        //when
+        int a = 10;
+        int b = 5;
+        //then
+        Assert.assertEquals(2, calculator.div(a, b), 0);
+    }
+}
