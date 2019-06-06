4d424dbe29164346b9615e6babdef8586d7d2287
niokle <myszapl@gmail.com>
2019-06-06 11:40:47 +0200

    task 24.1 commit 3

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/dao/CompanyDaoFacade.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/dao/CompanyDaoFacade.java
@@ -2,7 +2,7 @@ package com.kodilla.hibernate.manytomany.facade.dao;
 
 import com.kodilla.hibernate.manytomany.Company;
 import org.hibernate.annotations.NamedQuery;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,10 +10,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@EnableJpaRepositories("com.kodilla.hibernate")
 @Transactional
 @Repository
 @NamedQuery(name="Company.findByName", query="SELECT * FROM COMPANIES WHERE COMPANY_NAME LIKE '%' + :ARG + '%'")
 public interface CompanyDaoFacade extends CrudRepository<Company, Integer> {
-    @Query
     List<Company> findCompanyByNameContains(@Param("ARG") String containsChars);
 }
--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/dao/EmployeeDaoFacade.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/dao/EmployeeDaoFacade.java
@@ -2,6 +2,7 @@ package com.kodilla.hibernate.manytomany.facade.dao;
 
 import com.kodilla.hibernate.manytomany.Employee;
 import org.hibernate.annotations.NamedQuery;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@EnableJpaRepositories("com.kodilla.hibernate")
 @Transactional
 @Repository
 @NamedQuery(name = "Employee.findByName", query = "SELECT * FROM employees WHERE CONCAT(firstname, lastname) LIKE '%' + :ARG + '%'")
